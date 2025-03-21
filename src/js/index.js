abad36a2faaf36e4f8426ade1c2e138b88cd8eee
Robson Svicero <robsonsvicero@outlook.com>
2025-03-21 17:01:18 -0300

    commit-21-03-2025

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const whatsappButton = document.querySelector(".whatsapp-button");
 
   window.addEventListener("scroll", function() {
-      if (window.scrollY > 200) {
+      if (window.scrollY > 180) {
           whatsappButton.style.display = "flex";
       } else {
           whatsappButton.style.display = "none";
